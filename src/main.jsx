e005361983bab0b754da272a1d06602b11edfbf1
Zülal Nebin <zllnbn1997@gmail.com>
2024-10-05 19:12:09 +0300

    setup apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import client from "./apollo.js";
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8000,
+  },
+  resolve: {
+    alias: {
+      pages: resolve(__dirname, "./src/pages"),
+      components: resolve(__dirname, "./src/components"),
+    },
+  },
+});
